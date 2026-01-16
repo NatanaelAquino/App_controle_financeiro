fad185f4ef0eba79d057cd170a05ab4f0785a7a3
Natanael Aquino <wedit62@gmail.com>
2026-01-15 22:31:53 -0300

    chore: update TypeScript from v18 to v19

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,12 +5,7 @@ import { fontFamily } from "@/theme/fontFamily";
 export default function Index() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10 }}>
-            <Text style={{ fontFamily: fontFamily.bold}}>Home</Text>
-
-            <Button title="NOVA META" onPress={() => router.navigate("/target")} />
-            <Button title="transação" onPress={() => router.navigate("/transaction/1")} />
-            <Button title="Progresso" onPress={() => router.navigate("/in-progress/12")} />
-
+            
         </View>
     )
 } 
---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { styles } from "./styles";
+import {LinearGradient} from 'expo-linear-gradient';
+
+
+export function HomeHeader() {
+    return (
+        <View style={styles.container} >    
+        
+        </View>      
+    )
+}
