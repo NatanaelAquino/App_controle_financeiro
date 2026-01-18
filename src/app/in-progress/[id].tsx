bdf0177c5092580d013ada6f655bbb0cf1796deb
Natanael Aquino <wedit62@gmail.com>
2026-01-17 22:43:23 -0300

    feat: estrutara de meta em andamento

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,18 +1,56 @@
+import { View, } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
-import {View, Button, Text } from "react-native";
+import { PageHeader } from "@/components/PageHeader/indes";
+import { Progress } from "@/components/Progress";
+import { Button } from "@/components/Butto";
+import { List } from "@/components/List/indes";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+import { TransactionType } from "@/utils/transactionTypes";
+import { use } from "react";
 
-type  TransactionType = {
-    id: string;
+const data = {
+    current: "R$ 580,00",
+    target: "R$ 1.790,00",
+    percentage: 25
 }
-export default function InProgress() {
-    const {id} = useLocalSearchParams<TransactionType>();
 
-   return (
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-              <Text>Transaction {id}</Text>
-  
-              <Button title="Voltar" onPress={() => router.back()} />
-  
-          </View>
-      );
-  }
+const Transactions: TransactionProps[] = [
+    {
+        id: "1",
+        value: "R$ 20,00",
+        date: "12/04/25",
+        description: "",
+        type: TransactionType.Output
+    },
+    {
+        id: "2",
+        value: "R$ 300,00",
+        date: "12/04/25",
+        description: "CDB de 110% no banco XPTO",
+        type: TransactionType.Input
+    },
+]
+
+export default function InProgress() {
+    const params = useLocalSearchParams<TransactionType>();
+    return (
+        <View style={{ flex: 1, padding: 24, gap: 24 }}>
+            <PageHeader title="Meta em andamento"
+                rightButton={{ icon: "edit", onPress: () => { } }}
+            />
+            <Progress data={data} />
+            <List
+                title="Transções" data={Transactions}
+                renderItem={({ item }) => (
+                    <Transaction data={item} onRemove={() => { }} />
+
+                )}
+                emptyMessage="Nenhuma transação. Toque em nova transação para guardar seu primeiro dinheiro aqui."
+            />
+            <Button
+                title="Nova transação"
+                onPress={() => { router.navigate(`/transaction/${params.id}`) }}
+            />
+        </View>
+    );
+}
--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,6 +1,8 @@
 import { Button, Text, View } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
 
+
+import { PageHeader } from "@/components/PageHeader/indes";
 type TransactionType = {
     id: string
 }
@@ -8,9 +10,11 @@ type TransactionType = {
 export default function Transaction() {
     const { id } = useLocalSearchParams<TransactionType>();
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Transaction {id}</Text>
-
+        <View style={{ flex: 1 }}>
+            <PageHeader 
+            title="Nova transação" 
+            subTitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
+            />
             <Button title="Voltar" onPress={() => router.back()} />
 
         </View>
